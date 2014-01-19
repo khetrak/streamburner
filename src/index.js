691ee49c0df4d00baece0725cd8253786e9fc970
Michael Morrison <mmorrison@pon4.net>
2014-01-19 06:34:37 -0600

    Delay load by one tick, fixes race condition with twitch chat

--- src/index.js
+++ src/index.js
@@ -7,14 +7,18 @@ if(!env.detect()) {
 }
 
 jquery.load(function($) {
+	function documentReady() {
+		logger.log("Document is ready, waiting one tick");
+		setTimeout(function() {
+			require('./run');
+		},0);
+	}
+
 	if(document.readyState == 'complete' || document.readyState == 'interactive') {
-		require('./run');
+		documentReady();
 	} else {
 		logger.log("Waiting for document to be ready...");
-		$(function() {
-			logger.log("Document is ready");
-			require('./run');
-		});
+		$(documentReady);
 	}
 	
 });
